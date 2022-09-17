11831d93c092f61d3c70d5b13fbd833fbad85d60
Hemcode <pro.hemdy@gmail.com>
2022-09-17 08:23:55 +0200

    JSlearn002

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const main = function() {
-  let a = [{
-    nom: "Hemdy",
-    age: 13,
-  }, {
-    nom: 'Keyvan',
-    age: 13,
-  }, {
-    nom: "Hamza",
-    age: 13,
-  }]
-  console.log(a[2].nom);
-}
-
-main();
+document.querySelector("body").style.backgroundColor = "azure";
